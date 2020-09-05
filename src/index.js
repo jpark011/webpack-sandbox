3cec82d79666a06741b9ebe17fbd1829229eed03
Jay Park <wngt3319@gmail.com>
2020-09-05 12:56:23 -0700

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import logo from './melon_logo.png';
+import data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = logo;
+
+    element.appendChild(myIcon);
+
+    console.log(data);
 
     return element;
 }
